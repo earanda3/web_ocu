89ad71caa886d1c45e6d652c5e2d8be54cf20184
TECLA-code <earanda1994@gmail.com>
2026-04-04 19:16:17 +0200

    t12

--- content/manifest.js
+++ content/manifest.js
@@ -10,16 +10,12 @@ window.CONTENT_MANIFEST = [
       "pdf": [],
       "image": [
         "Untitled_Artwork 2.png",
-        "Untitled_Artwork 201.jpg",
-        "Untitled_Artwork 3.png",
         "Untitled_Artwork 4.png",
         "Untitled_Artwork 5.png",
         "Untitled_Artwork 6.png",
         "Untitled_Artwork 9.png",
         "Untitled_Artwork copia 2.png",
-        "Untitled_Artwork copia.png",
-        "sinte.png",
-        "sinte01.png"
+        "Untitled_Artwork copia.png"
       ]
     }
   },
@@ -45,8 +41,17 @@ window.CONTENT_MANIFEST = [
     ],
     "files": {
       "stl": [
+        "Anell2.stl",
+        "Arzak2.stl",
         "Ocu3D2.stl",
         "TukTuk.stl",
+        "ocu3D10.stl",
+        "ocu3D13.stl",
+        "ocu3D16.stl",
+        "ocu3D4.stl",
+        "ocu3D5.stl",
+        "ocu3D6.stl",
+        "ocu3D7.stl",
         "shapr3d_export_2026-04-01_01h36m.stl",
         "shapr3d_export_2026-04-01_01h37m.stl"
       ],
